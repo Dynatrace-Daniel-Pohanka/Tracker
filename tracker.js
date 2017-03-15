62843de170720b0ce47de215ec7c72a7191c891a
Jacob Potter <pttr.jcb@gmail.com>
2017-03-14 21:09:21 -0400

    added slide toggle for settings

--- tracker.js
+++ tracker.js
@@ -1,19 +1,45 @@
 var rowCount = 0;
 $(document).ready(function() {
     $(".add-row").click(function() {
-        rowCount++;
-        var field=$("#fieldDef").val();
-        var value=$("#valueDef").val();
-        var markup = "<tr><td><input type='checkbox' name='record'></td><td><form><input type='text' name=field" + rowCount + " placeholder=test value="+field+"></form></td><td><form><input type='text' name=value" + rowCount + " placeholder=test2 value="+value+"></form></td></tr>";
-        $("table tbody").append(markup);
+        var field = $("#fieldDef").val();
+        var value = $("#valueDef").val();
+        $("table tbody")
+            .append($('<tr>')
+                .append($('<td>')
+                    .append($('<input>')
+                        .attr('type', 'checkbox')
+                        .attr('name', 'record')))
+                .append($('<td>')
+                    .append($('<form>')
+                        .append($('<input>')
+                            .attr('type', 'text')
+                            .attr('name', 'field')
+                            .attr('placeholder', 'test')
+                            .attr('value', field))))
+                .append($('<td>')
+                    .append($('<form>')
+                        .append($('<input>')
+                            .attr('type', 'text')
+                            .attr('name', 'field')
+                            .attr('placeholder', 'test2')
+                            .attr('value', value))))
+            );
     });
 
     // Find and remove selected table rows
     $(".delete-row").click(function() {
-        $("table tbody").find('input[name="record"]').each(function() {
+        $("table tbody").find('input[name="delete"]').each(function() {
             if ($(this).is(":checked")) {
                 $(this).parents("tr").remove();
             }
+            // else {
+            //   $('#table tr:last').remove();
+            // }
         });
     });
+
+    $(".toggle").click(function() {
+        $("#settings").slideToggle();
+    });
+
 });
