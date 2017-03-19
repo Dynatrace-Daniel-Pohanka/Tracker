6bfd64f4d1617b835c252dd727f2eddae9dddef4
Jacob Potter <pttr.jcb@gmail.com>
2017-03-18 22:55:31 -0400

    Added auto field update

--- tracker.js
+++ tracker.js
@@ -1,9 +1,22 @@
 var rowCount = 0;
+function update() {
+  var steps=parseInt(document.getElementById('steps').value);
+  var minutes=parseInt(document.getElementById('minutes').value);
+  var loc=parseInt(document.getElementById('loc').value);
+  var object=parseInt(document.getElementById('object').value);
+  var scoe=parseInt(document.getElementById('scoe').value);
+  var hours=parseInt(document.getElementById('hours').value);
+
+  var subtotal=document.getElementById('subtotal');
+  if (minutes>0&&steps>0&&loc>0&&hours>0) {
+  subtotal.value= steps*(60/minutes)*loc*hours;
+}
+}
 $(document).ready(function() {
     $(".add-row").click(function() {
         var field = $("#fieldDef").val();
         var value = $("#valueDef").val();
-        $("table tbody")
+        $(".tracker")
             .append($('<tr>')
                 .append($('<td>')
                     .append($('<input>')
@@ -14,15 +27,34 @@ $(document).ready(function() {
                         .append($('<input>')
                             .attr('type', 'text')
                             .attr('name', 'field')
-                            .attr('placeholder', 'test')
-                            .attr('value', field))))
+                            .attr('placeholder', '# of steps'))))
                 .append($('<td>')
                     .append($('<form>')
                         .append($('<input>')
                             .attr('type', 'text')
                             .attr('name', 'field')
-                            .attr('placeholder', 'test2')
-                            .attr('value', value))))
+                            .attr('placeholder', 'minutes'))))
+                .append($('<td>')
+                    .append($('<form>')
+                        .append($('<input>')
+                            .attr('type', 'text')
+                            .attr('name', 'field')
+                            .attr('placeholder', '# of loc.'))))
+                .append($('<td>')
+                    .append($('<input>')
+                        .attr('type', 'checkbox')
+                        .attr('name', 'object')))
+                .append($('<td>')
+                    .append($('<input>')
+                        .attr('type', 'checkbox')
+                        .attr('name', 'scoe')))
+                .append($('<td>')
+                    .append($('<form>')
+                        .append($('<input>')
+                            .attr('type', 'text')
+                            .attr('name', 'field')
+                            .attr('placeholder', 'hours'))))
+
             );
     });
 
@@ -39,7 +71,7 @@ $(document).ready(function() {
     });
 
     $(".toggle").click(function() {
-        $("#settings").slideToggle();
+        $(".settings").slideToggle();
     });
 
 });
