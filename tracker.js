06ccdbfaa2bbaf08daa492af6dbbc34dd93fffec
JacobPotter <pttr.jcb@gmail.com>
2017-03-21 09:56:39 -0400

    Row tracking for dynamic added rows with calcution. Initial setup, need to still add feature to append function

--- tracker.js
+++ tracker.js
@@ -1,13 +1,20 @@
-var rowCount = 0;
-function update() {
-  var steps=parseInt(document.getElementById('steps').value);
-  var minutes=parseInt(document.getElementById('minutes').value);
-  var loc=parseInt(document.getElementById('loc').value);
-  var object=parseInt(document.getElementById('object').value);
-  var scoe=parseInt(document.getElementById('scoe').value);
-  var hours=parseInt(document.getElementById('hours').value);
+function update(row) {
+  var stepsPlace='steps'+row;
+  var minutesPlace='minutes'+row;
+  var locPlace='loc'+row;
+  var objectPlace='object'+row;
+  var scoePlace='scoe'+row;
+  var hoursPlace='hours'+row;
+  var subtotalPlace='subtotal'+row;
 
-  var subtotal=document.getElementById('subtotal');
+  var steps=parseInt(document.getElementById(stepsPlace).value);
+  var minutes=parseInt(document.getElementById(minutesPlace).value);
+  var loc=parseInt(document.getElementById(locPlace).value);
+  var object=document.getElementById(objectPlace);
+  var scoe=document.getElementById(scoePlace).value;
+  var hours=parseInt(document.getElementById(hoursPlace).value);
+
+  var subtotal=document.getElementById(subtotalPlace);
   if (minutes>0&&steps>0&&loc>0&&hours>0) {
   subtotal.value= steps*(60/minutes)*loc*hours;
 }
